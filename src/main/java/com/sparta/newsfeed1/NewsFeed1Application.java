e76ce07207c3417ed058f595a5cacd91f5616ff3
whl564 <whl564@naver.com>
2023-07-02 09:57:10 +0900

    init commit

---
+++ src/main/java/com/sparta/newsfeed1/NewsFeed1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeed1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFeed1Application.class, args);
+	}
+
+}
